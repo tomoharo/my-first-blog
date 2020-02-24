c08a109955a522975b89da9da4deaeb8916b0e6e
ola <shisho88ZZZ@yahoo.co.jp>
2020-02-24 22:48:20 +0900

    Added views to create/edit blog post inside the site

---
+++ blog/forms.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from django import forms
+
+from .models import Post
+
+class PostForm(forms.ModelForm):
+
+    class Meta:
+        model = Post
+        fields = ('title', 'text',)
+
+if __name__ == "__main__":
+    pass
